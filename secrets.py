f8f1ddcd4fea12f33d72f773f9fbf35736f8430d
jainabou <40322777+jainabou@users.noreply.github.com>
2018-12-05 10:53:00 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,3 +0,0 @@
-ZWSID= 'X1-ZWz1gs1eclnvnv_6plsv'
-QUANDL_KEY='1xxGNJrQnbUrhHkeeyps'
-YELP_KEY='uLPuLhaokazVbiBSHK7T5StH9Z4B9OhtwrRzl_RtIyuo_gchMzyNEaH9MVVWLa3mbDCGgYRMgfUrU10PBBQYE275WmVNyuBWl4_Y3gUiIrQqfg09tn1t4z-EpwEGXHYx'
